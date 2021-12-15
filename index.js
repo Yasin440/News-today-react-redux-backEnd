2818392926b255dc72cede8dd8daa89271771120
Yasin440 <eiasin440@gmail.com>
2021-12-15 22:21:14 +0600

    connect MongoDB

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+//linked to mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nort6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 async function run() {
     try {
         await client.connect();
